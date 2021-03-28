088bfda43081f42cf9578afa5504c1d1294e2600
byeonhajin <gkwls1130@ajou.ac.kr>
2021-03-29 01:40:27 +0900

    Controller for test

---
+++ src/main/java/web/HelloController.java
@@ -0,0 +1,11 @@
+package web;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello") // http get 요청을 받을 수 있는 API 만들어줌
+    public String hello() {
+        return "hello";
+    }
+}
